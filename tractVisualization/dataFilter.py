4bdc74befee5bdd80d4b23dcd724703c9ed7c959
Roger1219 <ahumuroger@gmail.com>
2023-11-06 20:26:14 +0800

    extract 80% nodes of tract

---
+++ tractVisualization/dataFilter.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+# 读取Excel文件
+df = pd.read_excel('./tractVisualization/test.xlsx')
+
+# 提取node为10到90的行
+filtered_df = df[(df['node'] >= 11) & (df['node'] <= 90)]
+
+# 根据name和actName分组，计算平均值
+result_df = filtered_df.groupby(['name', 'tractName', 'group'])['values'].mean().reset_index()
+
+# 重命名列
+result_df.columns = ['name', 'actName', 'group', 'average_value']
+
+# 打印结果
+print(result_df)
--- tractVisualization/profile_extract.py
+++ tractVisualization/profile_extract.py
@@ -69,7 +69,16 @@ df_patient = profile_group(patientList, tractList, "patient")
 df_normal = profile_group(subjList, tractList, "normal")
 df = pd.concat([df_patient, df_normal], ignore_index=True)
 print (df)
-df.to_excel('~/text.xlsx', index=False)
+
+# 提取node为10到90的行
+filtered_df = df[(df['node'] >= 11) & (df['node'] <= 90)]
+# 根据name和actName分组，计算平均值
+result_df = filtered_df.groupby(['name', 'tractName', 'group'])['values'].mean().reset_index()
+# 重命名列
+result_df.columns = ['name', 'actName', 'group', 'average_value']
+# 打印结果
+print(result_df)
+result_df.to_excel('~/80PercentResult.xlsx', index=False)
 
 
 
